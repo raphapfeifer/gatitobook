4f47506de34fa08ae5b2aca887b4b42427ca7833
raphapfeifer <rapha_pfeifer@yahoo.com.br>
2022-04-02 15:42:38 -0300

    Acerto do formato do projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,9 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { HomeModule } from './home/home.module';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeModule } from './home/home.module';
 
 @NgModule({
   declarations: [
