5fb017abbb8e14cb409eb3085ea86b55fdcdd010
raphapfeifer <rapha_pfeifer@yahoo.com.br>
2022-05-09 01:01:55 -0300

    Método para cadastrar usuário

--- src/app/home/novo-usuario/novo-usuario.component.ts
+++ src/app/home/novo-usuario/novo-usuario.component.ts
@@ -5,6 +5,7 @@ import { FormBuilder,FormGroup, Validators } from '@angular/forms';
 import { NovoUsuario } from './novo-usuario';
 import { minusculoValidator } from './minusculo.validator';
 import { usuarioSenhaIguaisValidator } from './usuario-senha-iguais-validator';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-novo-usuario',
@@ -17,7 +18,8 @@ export class NovoUsuarioComponent implements OnInit {
 
   constructor(private formBuilder:FormBuilder,
      private NovoUsuarioService: NovoUsuarioService,
-     private usuarioExisteService: UsuarioExisteService
+     private usuarioExisteService: UsuarioExisteService,
+     private router: Router
      ) { }
 
   ngOnInit(): void {
@@ -34,8 +36,19 @@ export class NovoUsuarioComponent implements OnInit {
   }
 
   cadastrar(){
-    const novoUsuario = this.novoUsuarioForm.getRawValue() as NovoUsuario;
-    console.log(novoUsuario);
+     if(this.novoUsuarioForm.valid){
+        const novoUsuario = this.novoUsuarioForm.getRawValue() as NovoUsuario;
+        this.NovoUsuarioService.cadastraUsuario(novoUsuario).subscribe(() => {
+          this.router.navigate(['']);
+        },
+          (error) => {
+            console.log(error);
+          }
+        );
+
+
+
+     }
   }
 
 }
