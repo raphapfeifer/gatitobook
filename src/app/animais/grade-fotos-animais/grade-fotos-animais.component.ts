6f5ec3efdcbedc64d32df3ed63232a859c92d5ff
raphapfeifer <rapha_pfeifer@yahoo.com.br>
2022-06-06 01:05:06 -0300

    Criação de componente grade de animais

---
+++ src/app/animais/grade-fotos-animais/grade-fotos-animais.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-grade-fotos-animais',
+  templateUrl: './grade-fotos-animais.component.html',
+  styleUrls: ['./grade-fotos-animais.component.css']
+})
+export class GradeFotosAnimaisComponent implements OnInit {
+
+  @Input() animais!: Animais;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
