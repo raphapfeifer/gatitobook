54252fee657743a1152b8a0232dac5f19981fd25
raphapfeifer <rapha_pfeifer@yahoo.com.br>
2022-06-13 02:15:36 -0300

    guard de rotas

---
+++ src/app/autenticacao/autenticacao.guard.ts
@@ -0,0 +1,26 @@
+import { UsuarioService } from './usuario/usuario.service';
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AutenticacaoGuard implements CanActivate {
+
+  constructor(private usuarioService: UsuarioService, private router: Router){}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+
+      if(!this.usuarioService.estaLogado()){
+          this.router.navigate(['']);
+          return false;
+      }
+
+      return true;
+  }
+
+}
