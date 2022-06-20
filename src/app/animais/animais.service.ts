44a1a65eb1b8bd797c4201bfb7458a1a1379c215
raphael.alcateia <raphael.alcateia@salcisne.com.br>
2022-06-20 15:07:19 -0300

    Interação do detalhe do gato com o back

--- src/app/animais/animais.service.ts
+++ src/app/animais/animais.service.ts
@@ -1,7 +1,7 @@
 import { TokenService } from './../autenticacao/token.service';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Animais } from './animais';
+import { Animais, Animal } from './animais';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
@@ -21,6 +21,12 @@ export class AnimaisService {
     return this.http.get<Animais>(`${API}/${nomeDoUsuario}/photos`, {
       headers,
     });
+  }
+
+  buscaPorID(id:number):Observable<Animal>{
+    const token = this.tokenService.retornaToken();
+    const headers = new HttpHeaders().append('x-access-token', token);
+    return this.http.get<Animal>(`${API}/photos/${id}`, {headers});
   }
 
 }
