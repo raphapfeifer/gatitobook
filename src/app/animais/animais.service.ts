0a9ba48328f62bdec6ac7987279afc08051e7164
raphael.alcateia <raphael.alcateia@salcisne.com.br>
2022-06-24 15:35:36 -0300

    Criação de métodos de curtir e excluir

--- src/app/animais/animais.service.ts
+++ src/app/animais/animais.service.ts
@@ -2,11 +2,12 @@ import { TokenService } from './../autenticacao/token.service';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Animais, Animal } from './animais';
-import { Observable } from 'rxjs';
+import { catchError, Observable, throwError } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
 
 const API = environment.apiURL;
+const NOT_MODIFIED = '304';
 
 @Injectable({
   providedIn: 'root'
@@ -22,5 +23,21 @@ export class AnimaisService {
   buscaPorID(id:number):Observable<Animal>{
     return this.http.get<Animal>(`${API}/photos/${id}`);
   }
+
+  excluiAnimal(id:number):Observable<Animal>{
+    return this.http.delete<Animal>(`${API}/photos/${id}`);
+  }
+
+  curtir(id:number):Observable<boolean>{
+    return this.http.post(`${API}/photos/${id}/likes`, {},
+     {observe: 'response'}
+     ).pipe(
+      mapTo(true), 
+        catchError((error) => {
+          return error.status === NOT_MODIFIED ? of(false) : throwError(error);
+      })
+      );
+  }
+
 
 }
