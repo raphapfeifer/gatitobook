f4b096e2a1da767751440c82acf4b29b81e5e974
raphapfeifer <rapha_pfeifer@yahoo.com.br>
2022-07-10 23:28:44 -0300

    Criação do shared module

--- src/app/animais/animais.module.ts
+++ src/app/animais/animais.module.ts
@@ -1,5 +1,4 @@
-import { ReactiveFormsModule } from '@angular/forms';
-import { MensagemModule } from './../componentes/mensagem/mensagem.module';
+
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
@@ -10,6 +9,7 @@ import { CartaoModule } from '../componentes/cartao/cartao.module';
 import { GradeFotosAnimaisComponent } from './grade-fotos-animais/grade-fotos-animais.component';
 import { DetalheAnimalComponent } from './detalhe-animal/detalhe-animal.component';
 import { ComentariosComponent } from './detalhe-animal/comentarios/comentarios.component';
+import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
@@ -23,8 +23,7 @@ import { ComentariosComponent } from './detalhe-animal/comentarios/comentarios.c
     CommonModule,
     AnimaisRoutingModule,
     CartaoModule,
-    MensagemModule,
-    ReactiveFormsModule
+    SharedModule
   ],
 })
 export class AnimaisModule { }
