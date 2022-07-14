245ff3ce946d22d62b5d3c1a68db930573903b1d
raphael.alcateia <raphael.alcateia@salcisne.com.br>
2022-07-14 16:45:57 -0300

    Inclusão de menu no cabeçalho

--- src/app/componentes/cabecalho/cabecalho.module.ts
+++ src/app/componentes/cabecalho/cabecalho.module.ts
@@ -2,6 +2,8 @@ import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CabecalhoComponent } from './cabecalho.component';
+import { MenuComponent } from '../menu/menu.component';
+import { MenuModule } from '../menu/menu.module';
 
 
 
@@ -9,7 +11,7 @@ import { CabecalhoComponent } from './cabecalho.component';
   declarations: [
     CabecalhoComponent
   ],
-  imports: [CommonModule,RouterModule],
+  imports: [CommonModule,RouterModule,MenuModule],
   exports: [CabecalhoComponent],
 })
 export class CabecalhoModule { }
