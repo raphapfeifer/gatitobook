c60382dec78318be485804b4b7b88499066fd603
raphapfeifer <rapha_pfeifer@yahoo.com.br>
2022-07-19 00:39:48 -0300

    template do upload

--- src/app/animais/novo-animal/novo-animal.component.ts
+++ src/app/animais/novo-animal/novo-animal.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-novo-animal',
@@ -6,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./novo-animal.component.css']
 })
 export class NovoAnimalComponent implements OnInit {
+  formularioAnimal!: FormGroup;
+  file!: File;
+  preview!: string;
+  percentualConcluido = 0;
 
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  upload(){}
+
+  gravaEvento(arquivo: any){}
 
 }
